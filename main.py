4691fa7a49724c3b392a8abf7369d97bc5057734
cliff-4 <adityamishra0700@gmail.com>
2023-12-30 15:41:45 +0530

    everything works finally

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import local_tokens
 from transformers import pipeline
 import langchain
+langchain.verbose = False
+import os
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
 
 # Your Hugging Face API token
 huggingface_token = local_tokens.api_keys()["HF"]["INIT_READ"]
@@ -16,15 +21,10 @@ postprocessor = langchain.postprocessors.QuestionPostprocessor()
 chain = langchain.prompts.langchain.chains.ComposableChain([prompt, question_generator, postprocessor])
 
 def generate_questions(text):
-    # Generate questions based on the input text
-    return chain.run(text)
+  # Generate questions based on the input text
+  return chain.run(text)
 
 # Example usage
 input_text = "Your input paragraph here."
 questions = generate_questions(input_text)
 print(questions)
-
-from transformers import pipeline
-print("Transformers library has been installed successfully!")
-
-
